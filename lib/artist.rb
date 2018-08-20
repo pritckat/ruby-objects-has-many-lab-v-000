9b7265b2b547097ddeb7a1ede93e4629cbf6024e
Katrina Pritchard <pritckat@gmail.com>
2018-08-20 17:23:01 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,10 +1,14 @@
 class Artist
   attr_accessor :name
   attr_reader :songs
-
+  @@songs = []
   def initialize(name)
     @name = name
     @songs = []
   end
 
+  def add_song
+    song = Song.new
+    song.artist = self
+  end
 end
